db1d72c311f0d39d7f9524833584a222c7eea998
sonjaove <the.seven.year.old.ove@gmail.com>
2024-06-28 17:39:33 +0530

    the code is interpolating rainfall values for 40 years at fro 10km resolution

--- course_work/dump.py
+++ course_work/dump.py
@@ -110,3 +110,28 @@ def res_change(minx,miny,maxx,maxy,input_res,output_res):
 
 if __name__ == "__main__":
     res_change(minx, miny, maxx, maxy, 0.25, 0.1)
+
+
+# a small trick i wa trying to do, to get all the data for all the years together.
+
+#repeating the lat, lon for 40 years.
+start_date = '1984-01-01'
+end_date = '2023-12-31'
+date_range = pd.date_range(start=start_date, end=end_date, freq='D')
+dates_df = pd.DataFrame({'date': date_range})
+n_dates = len(date_range)
+df_rep = pd.concat([inter_points]*n_dates, ignore_index=True)
+df_rep['date']=np.tile(date_range, len(inter_points))
+df_rep = df_rep[['date', 'lon', 'lat']]
+df_rep
+
+
+# interpolation using a diffrent class from scipy
+
+def interpolate(initial_res_points, data, coords, method_used):
+    # Ensure the entered coordinates have accuracy of only 1 digit after the decimal.
+    if method_used in ['linear', 'nearest', 'cubic']:
+        interpolator = sci.RegularGridInterpolator(initial_res_points, data, method=method_used)
+        return interpolator(coords)
+    else:
+        raise ValueError("Invalid method. Supported methods are 'linear', 'nearest', and 'cubic'.")
